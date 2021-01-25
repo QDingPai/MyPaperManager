70671869ae87fb812fe5609cad2bd4b527c4156a
paipai2333 <rffgratpnq@privaterelay.appleid.com>
2021-01-25 18:51:32 +0800

    1 ResultVO

---
+++ src/main/java/com/nefu/se/graduationprocessmanagement/vo/ResultVO.java
@@ -0,0 +1,9 @@
+package com.nefu.se.graduationprocessmanagement.vo;
+
+import java.util.Map;
+
+public class ResultVO {
+    private Integer code;
+    private String message;
+    private Map<String, Object> data;
+}

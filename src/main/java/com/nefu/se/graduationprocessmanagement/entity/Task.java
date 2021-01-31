595c206ef84428beb37b029b24c4dfb68222499e
paipai2333 <rffgratpnq@privaterelay.appleid.com>
2021-01-31 11:46:50 +0800

    Admin修正1

---
+++ src/main/java/com/nefu/se/graduationprocessmanagement/entity/Task.java
@@ -0,0 +1,37 @@
+package com.nefu.se.graduationprocessmanagement.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@TableName("task")
+public class Task {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private Integer type;
+
+    private Integer status;
+
+    private Integer target;
+
+    private Long teacherId;
+
+    private String filename;
+
+    private String uploadRegexName;
+
+    private String extName;
+
+    private LocalDateTime updateTime;
+}
